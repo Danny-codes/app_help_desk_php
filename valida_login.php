f64a1d1aec94976b532480555591f0d9b9bf89d7
Daniel Reis <danielreisp.2020@gmail.com>
2021-08-16 21:27:16 -0300

    Aplicando restrições em consulta de chamado

--- valida_login.php
+++ valida_login.php
@@ -7,11 +7,17 @@
 
     //verifica se a autentificação foi realizada
     $usuario_autentificado = false;
+    $usuario_id = null;
+    $usuario_perfil_id = null;
+
+    $perfis = [1 => 'Administrativo', 2 => 'Usuário'];
 
     //usuarios do sistema
     $usuarios_app = [
-        ['email' => 'adm@teste.com.br', 'senha' => '123456'],
-        ['email' => 'user@teste.com.br', 'senha' => 'abcd'],
+        ['id' => 1,'email' => 'adm@teste.com.br', 'senha' => '123456', 'perfil_id' => 1],
+        ['id' => 2,'email' => 'user@teste.com.br', 'senha' => '123456','perfil_id' => 1],
+        ['id' => 3,'email' => 'jose@teste.com.br', 'senha' => 'abcd','perfil_id' => 2],
+        ['id' => 4,'email' => 'paula@teste.com.br', 'senha' => 'abcd','perfil_id' => 2]
     ];
 
     /*
@@ -24,12 +30,16 @@
 
         if($user['email'] ==  $_POST['email'] && $user['senha'] ==  $_POST['senha']){
             $usuario_autentificado = true;
+            $usuario_id = $user['id'];
+            $usuario_perfil_id = $user['perfil_id'];
         };
 
     }
     
     if($usuario_autentificado){
         $_SESSION['autentificado'] = 'SIM';
+        $_SESSION['id'] = $usuario_id;
+        $_SESSION['perfil_id'] = $usuario_perfil_id;
         header('Location: home.php');
     }else{
         $_SESSION['autentificado'] = 'NAO';
